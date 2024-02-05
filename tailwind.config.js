23a5394b075750e46331c29d51ae84ce381c15fa
SoUrAv <aansourav@gmail.com>
2024-02-05 19:00:52 +0600

    Responsiveness complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "view.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["*.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
