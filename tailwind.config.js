2d89304eb0ecd9f5a602cc159c13126826391da7
SoUrAv <aansourav@gmail.com>
2024-02-05 12:40:54 +0600

    Post view added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["index.html", "view.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
